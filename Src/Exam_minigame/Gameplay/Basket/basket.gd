ec471bd9943462c71cacab575204a2314a1e2916
atom10 <atom10.kw10@gmail.com>
2024-03-14 21:42:51 +0100

    Add mouse control to basket

--- Src/Exam_minigame/Gameplay/Basket/basket.gd
+++ Src/Exam_minigame/Gameplay/Basket/basket.gd
@@ -2,16 +2,20 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+var direction
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var mouse_position
 @export var exam_paper: TextureRect
+@export var contol_method: Control_method
 
 func _physics_process(_delta):
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	move_and_slide()
+	if contol_method == Control_method.keyboard:
+		direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		move_and_slide()
 
 
 func _on_area_2d_body_entered(body):
@@ -19,3 +23,12 @@ func _on_area_2d_body_entered(body):
 		exam_paper.submit_answear(body.Get_identity())
 		body.queue_free()
 	
+func _process(delta):
+	if contol_method == Control_method.mouse:
+		mouse_position = get_viewport().get_mouse_position()
+		position.x = mouse_position.x
+
+enum Control_method {
+	mouse,
+	keyboard
+}
